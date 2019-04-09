a895295d8107308c0e42bb0dcbb2c7388d25d70c
Ahmed Mahmoud <ahmedmk1812@gmail.com>
2019-04-09 18:58:08 +0200

    first commit

---
+++ desktop/src/com/oop/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oop.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oop.platformer.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
