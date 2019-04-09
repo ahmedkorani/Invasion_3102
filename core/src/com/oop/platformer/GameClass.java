1103a44d6bef528242f74b101c150403df8de4d1
Ahmed Mahmoud <ahmedmk1812@gmail.com>
2019-04-09 21:42:37 +0200

    loading maps

--- core/src/com/oop/platformer/GameClass.java
+++ core/src/com/oop/platformer/GameClass.java
@@ -2,34 +2,34 @@
 package com.oop.platformer;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.oop.platformer.Screens.Level1;
 
-public class GameClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class GameClass extends Game {
+
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	public static final float PPM = 100;
+
+	public SpriteBatch batch;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new Level1(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-		//Andrew's commit
+		super.render();
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/oop/platformer/Screens/Level1.java
@@ -0,0 +1,106 @@
+package com.oop.platformer.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.oop.platformer.GameClass;
+
+public class Level1 implements Screen {
+
+    private GameClass game; //instance of Game Class to view it through setScreen in GameClass.java
+
+    private OrthographicCamera gameCam; //game camera instance to move with the player character
+
+    private Viewport gamePort; //Manages a Camera and determines how world coordinates are mapped to and from the screen.
+
+    private TmxMapLoader mapLoader; //loads the level from assets
+
+    private TiledMap map;//reference for the map itself
+
+    private OrthogonalTiledMapRenderer renderer;
+
+    public Level1(GameClass game){
+
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(game.V_WIDTH,game.V_HEIGHT,gameCam);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("/home/ahmed/Desktop/Platformer/core/assets/Map/untitled.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2,0);
+    }
+
+    //User Input handling function
+    public void handleInput(float deltaTime){
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            gameCam.position.x += 150*deltaTime;
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            gameCam.position.x -= 150*deltaTime;
+    }
+
+
+    //update the game state
+    public void update(float deltaTime){
+
+        handleInput(deltaTime);
+        gameCam.update();
+        renderer.setView(gameCam); //tells our renderer to draw only what camera can see in our game world
+
+    }
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        //separate our update logic from render
+        update(delta);
+
+        //Clear the game screen with Black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //render our game map
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
+}
