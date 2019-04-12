0a4c1c1dba9c33b38c056f5d4f19aef7becd9246
Satharus <a.elmayyah@gmail.com>
2019-04-12 14:45:23 +0200

    Added Box2d objects for the floor is level1

--- core/src/com/oop/platformer/Screens/Level1.java
+++ core/src/com/oop/platformer/Screens/Level1.java
@@ -5,9 +5,14 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -30,6 +35,9 @@ public class Level1 implements Screen {
     private  Hud hud;
     private OrthogonalTiledMapRenderer renderer;
 
+    private World world1;
+    private Box2DDebugRenderer level1Floorb2dr;
+
     public Level1(){
 
         gameCam = new OrthographicCamera();
@@ -41,28 +49,47 @@ public class Level1 implements Screen {
         hud = new Hud(GameClass.batch);
         renderer = new OrthogonalTiledMapRenderer(map);
         gameCam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2,0);
+        world1 = new World(new Vector2(0,0), true);
+
+        level1Floorb2dr = new Box2DDebugRenderer();
+        BodyDef floorBodyDef = new BodyDef();
+        PolygonShape floorShape = new PolygonShape();
+        FixtureDef floorFixtureDef = new FixtureDef();
+        Body floor;
+
+        for (MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            floorBodyDef.type = BodyDef.BodyType.StaticBody;
+            floorBodyDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            floor = world1.createBody(floorBodyDef);
+
+            floorShape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            floorFixtureDef.shape = floorShape;
+            floor.createFixture(floorFixtureDef);
+        }
     }
 
     //User Input handling function
     public void handleInput(float deltaTime){
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Math.ceil(gameCam.position.x) <= 1407)
             gameCam.position.x += 150 * deltaTime;
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && Math.floor(gameCam.position.x) != 225)
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && Math.floor(gameCam.position.x) > 225)
             gameCam.position.x -= 150*deltaTime;
         if (Gdx.input.isKeyPressed(Input.Keys.F3))
             Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
             Gdx.graphics.setWindowedMode(GameClass.V_WIDTH*2, GameClass.V_HEIGHT*2);
-
     }
 
 
     //update the game state
     public void update(float deltaTime){
+        //System.out.printf("%f\n", gameCam.position.x);
         handleInput(deltaTime);
         gameCam.update();
         renderer.setView(gameCam); //tells our renderer to draw only what camera can see in our game world
-
     }
 
 
@@ -88,6 +115,8 @@ public class Level1 implements Screen {
 
         //render our game map
         renderer.render();
+
+        level1Floorb2dr.render(world1, gameCam.combined);
 
         GameClass.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
