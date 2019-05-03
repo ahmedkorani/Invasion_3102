43e220a56062b9c1bc94e9232a16c8d1b384322d
Satharus <a.elmayyah@gmail.com>
2019-05-03 08:31:16 +0200

    Added icon.
    Added icon files, as well as the .xcf source for edits.
    Modified the LwjglApplicationConfiguration object to set the icon and
    set the window title to "Invasion 3102"

--- desktop/src/com/oop/platformer/desktop/DesktopLauncher.java
+++ desktop/src/com/oop/platformer/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.oop.platformer.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.oop.platformer.GameClass;
@@ -10,17 +11,22 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        config.addIcon("Icon/Icon128.png", Files.FileType.Internal);
+        config.addIcon("Icon/Icon64.png", Files.FileType.Internal);
+        config.addIcon("Icon/Icon32.png", Files.FileType.Internal);
+        config.title = "Invasion 3102";
         config.width = (int) screenSize.getWidth();
         config.height = (int) screenSize.getHeight();
-
-// 		java - get screen size using the Toolkit class
-//		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-//		config.width = (int)screenSize.getWidth();
-//		config.height = (int)screenSize.getHeight();
-//		config.backgroundFPS = 60;
-//		config.foregroundFPS = 60;
-//		config.resizable = false;
-//		config.fullscreen = true;
+        /*
+ 		java - get screen size using the Toolkit class
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		config.width = (int)screenSize.getWidth();
+		config.height = (int)screenSize.getHeight();
+		config.backgroundFPS = 60;
+		config.foregroundFPS = 60;
+		config.resizable = false;
+		config.fullscreen = true;
+		*/
 
         new LwjglApplication(new GameClass((float) config.width, (float) config.height), config);
     }
